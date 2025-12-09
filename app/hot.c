9f3fb86a88b13aff5cbff89a7f5eb48221149614
Tom Smeets <tom@tsmeets.nl>
2025-12-09 22:42:57 +0100

    hot: use file watcher

--- app/hot.c
+++ app/hot.c
@@ -6,7 +6,9 @@
 static Memory *mem;
 static Memory *tmp;
 static u32 counter = 0;
+
 static Hot *hot;
+static File *watch;
 
 static i32 hot_compile(char *input_path, char *output_path) {
     Fmt *fmt = fmt_new(tmp);
@@ -22,11 +24,16 @@ static i32 hot_compile(char *input_path, char *output_path) {
 }
 
 void os_main(u32 argc, char **argv) {
+    bool do_init = false;
     tmp = mem_new();
 
     if (!hot) {
+        do_init = true;
         mem = mem_new();
         hot = hot_new(mem);
+        watch = os_watch_new();
+        os_watch_add(watch, "src");
+        os_watch_add(watch, "app");
 
         if (argc < 2) {
             fmt_s(ferr, "Usage: ");
@@ -35,11 +42,20 @@ void os_main(u32 argc, char **argv) {
             os_exit(1);
         }
     }
-    char *source_file = argv[1];
-    fmt_ss(ferr, "Source: ", source_file, "\n");
-    char *output_path = hot_mktmp(hot, "/tmp/main");
-    i32 ret = hot_compile(source_file, output_path);
-    hot_load(hot, output_path);
-    hot_call(hot, argc - 1, argv + 1);
+
+    if (os_watch_check(watch) || do_init) {
+        hot->os_main = 0;
+        char *source_file = argv[1];
+        fmt_ss(ferr, "Source: ", source_file, "\n");
+        char *output_path = hot_mktmp(hot, "/tmp/main");
+        i32 ret = hot_compile(source_file, output_path);
+        if (ret == 0) hot_load(hot, output_path);
+    }
+
+    if (hot->os_main) {
+        hot->os_main(argc - 1, argv + 1);
+    } else {
+        os_sleep(100 * 1000);
+    }
     mem_free(tmp);
 }
