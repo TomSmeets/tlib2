4d93995d3b85bc8ed041791588a608b134dda549
Tom Smeets <tom@tsmeets.nl>
2026-02-13 20:58:19 +0100

    gzip: cleanup

--- core/gzip.h
+++ core/gzip.h
@@ -3,27 +3,21 @@
 #include "fmt.h"
 #include "base64.h"
 
-// static Buffer gzip_extract(Memory *mem, Buffer data) {
-// }
+static void gzip_extract(Memory *mem, Buffer data) {
+}
 
 
 
 static void gzip_test(void) {
     Memory *mem = mem_new();
-    char *target =
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello wor\nld hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello"
-        "hello hello world hello hello";
+    fmt_s(fout, "Target:\n");
+    char *target = "hello hello world hello hello";
     fmt_hexdump(fout, str_buf(target));
-    // Buffer compressed = base64_decode(mem, str_buf("H4sIAAAAAAAAA8tIzcnJV8gAk+X5RTkpUDaY5AIAmdZcBR4AAAA="));
+
+    fmt_s(fout, "Compressed:\n");
+    Buffer compressed = base64_decode(mem, str_buf("H4sIAAAAAAAAA8tIzcnJV8gAk+X5RTkpUDaY5AIAmdZcBR4AAAA="));
+    fmt_hexdump(fout, compressed);
+
+    // Do something
+    gzip_extract(mem, compressed);
 }
