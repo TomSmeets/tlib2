c6c7f0658f8825944f8d26e933c9f09fe2798f6f
Tom Smeets <tom@tsmeets.nl>
2026-02-21 21:49:32 +0100

    gzip: support xfl flag

--- core/gzip.h
+++ core/gzip.h
@@ -27,8 +27,11 @@ static Buffer gzip_read(Memory *mem, Buffer data) {
     if (fextra != 0) return out;
 
     u32 mtime = stream_read_u32(&stream);
+
+    // XFL Compression info:
+    // 2 -> Best compression
+    // 4 -> Fast compression
     u8 xfl = stream_read_u8(&stream);
-    if (xfl != 0) return out;
 
     u8 os = stream_read_u8(&stream);
     while (fname && stream_read_u8(&stream));
@@ -52,17 +55,27 @@ static Buffer gzip_read(Memory *mem, Buffer data) {
 
 static void gzip_test(void) {
     Memory *mem = mem_new();
-    Buffer t1_target = str_buf("hello hello world hello hello\n");
-    Buffer t1_in = base64_decode(mem, str_buf("H4sIAAAAAAAAA8tIzcnJV8gAk+X5RTkpUDaY5AIAmdZcBR4AAAA="));
+    Buffer t0_target = str_buf("hello hello world hello hello\n");
+    Buffer t0_in = base64_decode(mem, str_buf("H4sIAAAAAAAAA8tIzcnJV8gAk+X5RTkpUDaY5AIAmdZcBR4AAAA="));
+    Buffer t0_out = gzip_read(mem, t0_in);
+    fmt_hexdump(fout, t0_target);
+    fmt_hexdump(fout, t0_out);
+    assert(buf_eq(t0_out, t0_target));
+
+    Buffer t1_target = base64_decode(mem, str_buf("GnSwX91w7Z9EqpaZeyPCIQ=="));
+    Buffer t1_in = base64_decode(mem, str_buf("H4sICHPOkWkAA2RhdGEAkyrZEH+34O18l1XTZlYrH1IEAFve5PUQAAAA"));
     Buffer t1_out = gzip_read(mem, t1_in);
     fmt_hexdump(fout, t1_target);
     fmt_hexdump(fout, t1_out);
     assert(buf_eq(t1_out, t1_target));
 
-    Buffer t0_target = base64_decode(mem, str_buf("GnSwX91w7Z9EqpaZeyPCIQ=="));
-    Buffer t0_in = base64_decode(mem, str_buf("H4sICHPOkWkAA2RhdGEAkyrZEH+34O18l1XTZlYrH1IEAFve5PUQAAAA"));
-    Buffer t0_out = gzip_read(mem, t0_in);
-    fmt_hexdump(fout, t0_target);
-    fmt_hexdump(fout, t0_out);
-    assert(buf_eq(t0_out, t0_target));
+    Buffer t3_target = str_buf("0000");
+    Buffer t3_in = base64_decode(mem, str_buf("H4sIAAAAAAACAw2PCQHAQAzCrERC6UfPv7FNAEkIRFI0w2KOhwIJJSrUaNAio0OPDPLfJFlkk0MuafLIRwUl6kcW1dRQS5k66tFBi076NzY99NKmj35MMGKSKeYPGmYZM8c8NlixyRbb7N+7rNljHw4snLhw48H/HePDj4sPGqJXT+gAAAA="));
+    Buffer t3_out = gzip_read(mem, t3_in);
+    fmt_hexdump(fout, t3_target);
+    fmt_hexdump(fout, t3_in);
+    fmt_hexdump(fout, t3_out);
+    fmt_flush(fout);
+    assert(buf_eq(t3_out, t3_target));
+   
 }
