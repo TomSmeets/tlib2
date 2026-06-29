747864f33fa1868977d5070b662b1aac1eb108b5
Tom Smeets <tom@tsmeets.nl>
2026-06-29 23:03:17 +0200

    http: init

--- app/http.c
+++ app/http.c
@@ -1,133 +1,88 @@
 #include "fmt.h"
+#include "io.h"
 #include "os_headers.h"
 #include "os_main.h"
-#include "time.h"
 
-static bool update = 1;
-static u64 value = 0;
-static bool init = 0;
+#if OS_LINUX
 
-#if OS_WASM
-void js_inc(void) {
-    value++;
-    update = 1;
+typedef enum {
+    AF_UNIX = 1,
+    AF_INET = 2,
+} sock_family;
+
+typedef enum {
+    SOCK_STREAM = 1,
+    SOCK_DGRAM = 2,
+} sock_type;
+
+static i64 linux_socket(sock_family family, sock_type type, int protocol) {
+    return linux_syscall3(0x29, family, type, protocol);
 }
 
-void js_dec(void) {
-    value--;
-    update = 1;
+typedef struct {
+    u16 sin_family;
+    u16 sin_port; // Big endian
+    u8 sin_addr[4];
+    u8 pad[16 - 8];
+} sockaddr_in;
+
+static i64 linux_bind(int fd, sockaddr_in *addr) {
+    return linux_syscall3(0x31, fd, (i64)(void *)addr, sizeof(sockaddr_in));
 }
+
+static i64 linux_listen(int fd, int backlog) {
+    return linux_syscall2(0x32, fd, backlog);
+}
+
+static i64 linux_accept(int fd, sockaddr_in *addr, int flags) {
+    int len = sizeof(sockaddr_in);
+    return linux_syscall4(0x120, fd, (intptr_t)addr, (intptr_t)&len, flags);
+}
+
+
+static u32 net_ip4(u8 a, u8 b, u8 c, u8 d) {
+    u32 res = 0;
+    res |= (u32)a << 0;
+    res |= (u32)b << 8;
+    res |= (u32)c << 16;
+    res |= (u32)d << 24;
+    return res;
+}
+
+static u16 net_port(u16 port) {
+    return (u16)((port >> 8) & 0xff) | ((port & 0xff) << 8);
+}
+
 #endif
 
-typedef struct Todo Todo;
-struct Todo {
-    char *text;
-    bool done;
-    Todo *next;
-    Todo *child;
-};
-
-static Todo *list = 0;
-
-static void list_add(char *task) {
-    Todo *t = mem_struct(mem_perm(), Todo);
-    t->text = task;
-    t->done = false;
-    t->next = list;
-    list = t;
-}
-
-static Todo *todo_new(char *text) {
-    Todo *t = mem_struct(mem_tmp(), Todo);
-    t->text = text;
-    t->done = false;
-    return t;
-}
-
-static char *str_copy(char *s) {
-    return mem_clone(mem_tmp(), s, str_len(s) + 1);
-}
-
-static Todo *todo_copy(Todo *todo) {
-    if (!todo) return 0;
-    Todo *new = mem_struct(mem_tmp(), Todo);
-    new->text = str_copy(todo->text);
-    new->done = todo->done;
-    new->next = todo_copy(todo->next);
-    new->child = todo_copy(todo->child);
-    return new;
-}
-
-static char *todo_fmt(Todo *todo) {
-    if (!todo) return "";
-    char *line = fstr(mem_tmp(), "Todo: ", todo->text, "");
-    char *child = todo_fmt(todo->child);
-    char *next = todo_fmt(todo->next);
-    return fstr(mem_tmp(), line, child, next);
-}
-
-typedef struct {
-    Write content;
-
-    u32 callback_count;
-    u32 callback_cap;
-    bool callback;
-} Html;
-
-typedef struct {
-    u32 index;
-    bool value;
-} HtmlCallback;
-
-static HtmlCallback html_callback(Html *html) {
-    int i = html->callback_count++;
-    return (HtmlCallback){i, false};
-}
-
-static void html_emit(Html *html, char *text) {
-    html->content = fmt(html->content, text);
-}
-
-#define fmt(...) ""
-
-static bool html_button(Html *html, char *text) {
-    HtmlCallback cb = html_callback(html);
-    html_emit(html, fmt("<button onclick=toggle(", cb.index, ")>"));
-    html_emit(html, "</button>");
+static u16 u16_swap(u16 x) {
+    return (x >> 8) | (x << 8);
 }
 
 static void os_main(void) {
-    print("Hello World: ", value);
-    // value++;
+    int fd = linux_socket(AF_INET, SOCK_STREAM, 0);
+    check(fd >= 0);
 
-    if (!init) {
-        init = 1;
-        js_append_style(R"(
-            button {
-                width:  100px;
-                height: 40px;
-            }
-        )");
-    }
+    // Initialize the details of the server socket
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_addr[0] = 127;
+    addr.sin_addr[1] = 0;
+    addr.sin_addr[2] = 0;
+    addr.sin_addr[3] = 1;
+    addr.sin_port = u16_swap(4444);
+    print("Bind: ", linux_bind(fd, &addr));
+    print("Listen: ", linux_listen(fd, 1));
 
-    if (update) {
-        if (value > 1000) value = 1000;
-
-        Fmt *f = fmt_new(mem_tmp());
-        fmt_g(f, "<h1>Hello World!</h1>\n");
-        fmt_g(f, "<p>Value: ", value, "</p>\n");
-        fmt_g(f, "<button onclick='tlib.import.js_dec()'>Dec</button>\n");
-        fmt_g(f, "<button onclick='tlib.import.js_inc()'>Inc</button>\n");
-
-        fmt_g(f, "<ul>\n");
-        for (u32 i = 0; i < value; ++i) {
-            fmt_g(f, "<li>", "i = ", i * i, "</li>\n");
-        }
-        fmt_g(f, "</ul>\n");
-
-        js_set_html((char *)fmt_end(f).data);
-        update = 0;
-    }
-
-    os_sleep(10 * TIME_MS);
+    sockaddr_in client_addr = {};
+    int client_fd = linux_accept(fd, &client_addr, 0);
+    print("Accept: ", client_fd);
+    print("Port: ", u16_swap(client_addr.sin_port));
+    print("Host: ", client_addr.sin_addr[0], ".", client_addr.sin_addr[1], ".", client_addr.sin_addr[2], ".", client_addr.sin_addr[3]);
+    char buffer[64];
+    sys_read(client_fd, buffer, sizeof(buffer));
+    sys_write(client_fd, buffer, sizeof(buffer));
+    sys_close(client_fd);
+    sys_close(fd);
+    os_exit();
 }
